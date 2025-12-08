52d8e4f9571672bf82ee30dde80a05c89e2939fc
Himanshu Kumar <himanshukumar.ledhupur@gmail.com>
2025-12-08 15:40:22 +0530

    Update

---
+++ sumofgiven.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,r,i=0,sum;
+    printf("Enter the number To find digits ");
+    scanf("%d",&n);
+     for 
+        (n>0;n=1;r=n%10);
+        sum=i*0+r;
+
+printf("The sum of digits is %d",sum);
+     
+     return 0;
+}
